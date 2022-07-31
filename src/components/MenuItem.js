41af62e705223f1d89714e594a1bc13049ba22ff
Abhi <bhatt23abhishek@gmail.com>
2022-07-31 14:38:26 +0530

    create MenuItem component

---
+++ src/components/MenuItem.js
@@ -0,0 +1,13 @@
+const MenuItem = (props) => {
+
+    const {itemName, activeMenuItem}=props;
+    // console.log(selectedItem);
+    return (
+        <div className={activeMenuItem? "menu-item active" : "menu-item"}>
+                <span>{itemName}</span>
+                <span><i style={activeMenuItem? {} : {display : 'none'}} className="fa-solid fa-angle-right"></i></span>  
+        </div>
+    );
+}
+
+export default MenuItem;
