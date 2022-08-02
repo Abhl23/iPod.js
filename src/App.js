7541b6edc7d283a1e33af7b4d7639c14575ce2fa
Abhi <bhatt23abhishek@gmail.com>
2022-08-02 19:23:48 +0530

    add play pause functionality

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ZingTouch from 'zingtouch';
 import $ from 'jquery';
 
+import song from './assets/theRide.mp3';
+
 import Screen from './components/Screen';
 import Wheel from './components/Wheel';
 
@@ -15,10 +17,16 @@ class App extends React.Component{
       isGamesVisible : false,
       isSettingsVisible : false,
       musicItems : ['All Songs', 'Artists', 'Albums'],
-      isMusicMenuVisible : false
+      isMusicMenuVisible : false,
+      isMusicPlayerVisible : false,
+      isMusicPlaying : false,
+      isAutoPlayDone : false
     };
     this.activeItem=0;
     this.angle=0;
+    this.audio=new Audio(song);
+    this.songTimestampId=undefined;
+    this.songBarId=undefined;
   }
 
   componentDidMount(){
@@ -60,7 +68,7 @@ class App extends React.Component{
 
   handleMenuButtonClick = () => {
 
-    const {isMusicMenuVisible}=this.state;
+    const {isMusicMenuVisible, isMusicPlayerVisible}=this.state;
 
     if(!isMusicMenuVisible){
       $('#side-menu').toggleClass('display-side-menu');
@@ -73,22 +81,35 @@ class App extends React.Component{
       isCoverflowVisible : false,
       isGamesVisible : false,
       isSettingsVisible : false,
-      isMusicMenuVisible : false
+      isMusicMenuVisible : (isMusicPlayerVisible? true : false),
+      isMusicPlayerVisible : false
     });
   };
 
   handleCentreButtonClick = () => {
 
-    const {activeMenuItem}=this.state;
+    const {activeMenuItem, isMusicMenuVisible, isAutoPlayDone}=this.state;
 
     $('#side-menu').removeClass('display-side-menu');
 
-    if(activeMenuItem===0){
+    if(isMusicMenuVisible && activeMenuItem===0){
+
+      if(!isAutoPlayDone){
+        this.handleAutoPlay();
+        return;
+      }
+
+      this.setState({
+        isMusicPlayerVisible : true,
+        isMusicMenuVisible : false
+      });
+    }
+    else if(!isMusicMenuVisible && activeMenuItem===0){
       this.setState({
         isCoverflowVisible : true
       });
     }
-    else if(activeMenuItem===1){
+    else if(!isMusicMenuVisible && activeMenuItem===1){
 
       this.activeItem=0;
 
@@ -97,7 +118,7 @@ class App extends React.Component{
         activeMenuItem : this.activeItem
       });
     }
-    else if(activeMenuItem===2){
+    else if(!isMusicMenuVisible && activeMenuItem===2){
       this.setState({
         isGamesVisible : true
       });
@@ -109,9 +130,103 @@ class App extends React.Component{
     }
   };
 
+  handleAutoPlay = () => {
+    this.audio.play();
+
+    this.setState({
+      isMusicPlayerVisible : true,
+      isMusicPlaying : true,
+      isAutoPlayDone : true,
+      isMusicMenuVisible : false
+    }, () => {
+      this.handleSongTimestamp();
+      this.handleSongBarWidth();
+    });
+  }
+
+  handleSongTimestamp = () => {
+    console.log('play');
+
+    const song=this.audio;
+    
+    this.songTimestampId=setInterval(() => {
+      let ct=Math.round(song.currentTime);
+
+      let minutes=parseInt(ct/60);
+      let seconds=parseInt(ct%60);
+
+      if(minutes===5 && seconds===51){
+        this.stopSong();
+      }
+
+      const timestamp=$('#song-timestamp');
+
+      timestamp.html((seconds < 10? `${minutes}:0${seconds}` : `${minutes}:${seconds}`));
+    }, 1000);
+  }
+
+  handleSongBarWidth = () => {
+    const song=this.audio;
+
+    this.songBarId=setInterval(() => {
+      let ct=Math.round(song.currentTime);
+      let tt=Math.round(song.duration);
+
+      let width=(ct*100)/tt;
+
+      const bar=$('#inner-bar');
+
+      bar.width(`${width}%`);
+    }, 100);
+  }
+
+  stopSong = () => {
+    clearInterval(this.songTimestampId);
+    clearInterval(this.songBarId);
+    this.setState({
+      isMusicPlaying : false,
+      isAutoPlayDone : false
+    });
+  }
+
+  handlePlayPause = () => {
+    const {isMusicPlaying, isAutoPlayDone}=this.state;
+
+    if(isMusicPlaying){
+      this.audio.pause();
+      console.log('pause');
+      clearInterval(this.songTimestampId);
+      clearInterval(this.songBarId);
+
+      this.setState({
+        isMusicPlaying : false
+      });
+    }
+    else if(isAutoPlayDone && !isMusicPlaying){
+      this.audio.play();
+
+      this.handleSongTimestamp();
+      this.handleSongBarWidth();
+
+      this.setState({
+          isMusicPlaying : true
+      });
+    }
+  }
+
   render(){
 
-    const {menuItems, activeMenuItem, musicItems, isCoverflowVisible, isGamesVisible, isSettingsVisible, isMusicMenuVisible}=this.state;
+    const {menuItems, 
+            activeMenuItem, 
+            musicItems, 
+            isCoverflowVisible, 
+            isGamesVisible, 
+            isSettingsVisible, 
+            isMusicMenuVisible,
+            isMusicPlayerVisible,
+            isMusicPlaying,
+            isAutoPlayDone
+          }=this.state;
 
     return (
       <div id="App">
@@ -121,10 +236,14 @@ class App extends React.Component{
           isCoverflowVisible={isCoverflowVisible}
           isGamesVisible={isGamesVisible}
           isSettingsVisible={isSettingsVisible}
+          isMusicPlayerVisible={isMusicPlayerVisible}
+          isMusicPlaying={isMusicPlaying}
+          isAutoPlayDone={isAutoPlayDone}
         />
         <Wheel
           onMenuButtonClick={this.handleMenuButtonClick}
           onCentreButtonClick={this.handleCentreButtonClick}
+          onPlayPause={this.handlePlayPause}
         />
       </div>
     );
